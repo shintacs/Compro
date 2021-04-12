ad6e28576b270b4bd13f5e89823701afda5bab1d
Ito Shintaro <71951771+shintacs@users.noreply.github.com>
2021-04-12 22:00:15 +0900

    Create hbd.cpp

---
+++ ABC100/hbd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    if(a < 9 && b < 9){
+        cout << "Yay!" << endl;
+    }else{
+        cout << ":(" << endl;
+    }
+}
