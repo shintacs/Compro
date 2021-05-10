5c208cf304054afdf7f34fbd16a895892a87ec3d
Ito Shintaro <71951771+shintacs@users.noreply.github.com>
2021-05-10 11:08:18 +0900

    Create a.cpp

---
+++ Contests/kyosera050821/a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if(n % 100 == 0)
+    {
+        cout << n / 100 << endl;
+    }
+    else
+    {
+        cout << n / 100 + 1 << endl;
+    }
+}
